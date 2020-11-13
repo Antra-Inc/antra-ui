f2cff40ad7103d0c42d99b8ff7a182bfd9df8014
show3567 <ydong011@fiu.edu>
2020-11-13 13:25:17 -0500

    add documents for sidenbar and sidebar interface

--- projects/antra-ui/src/lib/components/antra-sidenavbar/antra-sidenavbar.component.ts
+++ projects/antra-ui/src/lib/components/antra-sidenavbar/antra-sidenavbar.component.ts
@@ -3,25 +3,70 @@ import { MatTreeFlatDataSource, MatTreeFlattener } from '@angular/material/tree'
 import { FlatTreeControl } from '@angular/cdk/tree';
 import { NavLinkNode, NavLinkNodeFlat } from '../../interfaces/sidenavbar.interface';
 
+/**
+ * `antra-sidenavbar` is a vertical navigation component which apart from traditional, text links, might embed icons.
+ *
+ * By virtue of its clarity and simplicity, it remarkably increases the User Experience.
+ * It allows you to navigate through small applications as well as vast portals swiftly.
+ * Its multiple link embedding functionalities enables you to implement more advanced content categorization, which is almost essential within bigger projects.
+ *
+ * Thanks to Antra-UI you can easily implement SideNav in your own projects, by using one of various, alluring Side Menus.
+ *
+ * ### Usage
+ *  `import { AntraUiModule } from 'antra-ui'`
+ */
 @Component({
   selector: 'antra-sidenavbar',
   templateUrl: './antra-sidenavbar.component.html',
   styleUrls: ['./antra-sidenavbar.component.scss']
 })
+/**
+ * Example of usage:
+ * <example-url>http://127.0.0.1:4200/components/sidenav</example-url>
+ */
 export class SidenavbarComponent implements OnInit {
-
+  /**
+   * Value of control sidenavbar stretch;
+   */
   isExpanded = false;
 
-  @Input() sideNavTextColor = 'red'; //  customrize the sidenav text color;
-  @Input() sideNavBackground = ''; // customrize the sidenav sidenavbar color;
-  @Input() sidenavMode: 'over' | 'push' | 'side' = 'side'; // set sidenav mode;
-  @Input() treeNodePaddingIndent = '0px'; // set the subnode left padding;
-  @Input() isOpen = true; // set the side open or close;
-  @Input() containerClass = ''; // customrize sidenav container style;
+  /**
+   * Customrize the sidenav text color;
+   */
+  @Input() sideNavTextColor = 'red';
+  /**
+   * Customrize the sidenav sidenavbar color
+   */
+  @Input() sideNavBackground = '';
+  /**
+   * Set sidenav mode
+   */
+  @Input() sidenavMode: 'over' | 'push' | 'side' = 'side';
+  /**
+   * Set the subnode left padding
+   */
+  @Input() treeNodePaddingIndent = '0px';
+  /**
+   * Set the side open or close
+   */
+  @Input() isOpen = true;
+  /**
+   * Customrize sidenav container style
+   */
+  @Input() containerClass = '';
+  /**
+   * Customrize tree nodes, enables to implement more advanced content categorization
+   */
   @Input() sideNavConfig: NavLinkNode[] = [];
 
+  /**
+   * Event emiter, use to hold node info
+   */
   @Output() listOptionClicked = new EventEmitter();
 
+   /**
+    * @ignore
+    */
   // tslint:disable-next-line: variable-name
   private _transformer = (node: NavLinkNode, level: number) => {
     return {
@@ -32,13 +77,17 @@ export class SidenavbarComponent implements OnInit {
       level,
     };
   }
-
+   /**
+    * @ignore
+    */
   // tslint:disable-next-line: member-ordering
   treeControl = new FlatTreeControl<NavLinkNodeFlat>(
     (node) => node.level,
     (node) => node.expandable
   );
-
+   /**
+    * @ignore
+    */
   // tslint:disable-next-line: member-ordering
   treeFlattener = new MatTreeFlattener(
     this._transformer,
@@ -46,35 +95,58 @@ export class SidenavbarComponent implements OnInit {
     (node) => node.expandable,
     (node) => node.children
   );
+   /**
+    * @ignore
+    */
   // tslint:disable-next-line: member-ordering
   dataSource = new MatTreeFlatDataSource(this.treeControl, this.treeFlattener);
-
+   /**
+    * @ignore
+    */
   constructor() { }
-
+   /**
+    * @ignore
+    */
   ngOnInit(): void {
     this.dataSource.data = this.sideNavConfig;
   }
-
+   /**
+    * @ignore
+    */
   hasChild = (_: number, node: NavLinkNodeFlat) => node.expandable;
 
+  /**
+   * Emit event during click the tree node
+   * @param { NavLinkNodeFlat } node `node` contains customriz info for each tree node
+   */
   // tslint:disable-next-line: typedef
   handleListOnClick(node: NavLinkNodeFlat) {
     this.listOptionClicked.emit(node);
   }
 
+  /**
+   * Control sidenav expand
+   */
   handleExpanded(): void {
     this.isExpanded = !this.isExpanded;
   }
-
+  /**
+   * Handle mouse leave
+   */
   handleMouseLeave(): void {
     this.isExpanded = false;
   }
-
+  /**
+   * Handle mouse enter
+   */
   handleMouseEnter(): void {
     this.isExpanded = true;
   }
-
-  handleTreeNodeToggle(node): void {
+  /**
+   * Toggle the tree node which has child tree nodes
+   * @param { NavLinkNodeFlat } node
+   */
+  handleTreeNodeToggle(node: NavLinkNodeFlat): void {
     this.treeControl.toggle(node);
   }
 
