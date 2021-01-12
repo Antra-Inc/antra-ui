e3efeefad047c03de15d10ef846ecc393860ba6b
narendarlgentyala-antra <narendar.gentyala@antra.com>
2021-01-12 16:33:33 +0530

    Feature - login component update - test cases

--- projects/antra-ui/src/lib/components/login/login.component.spec.ts
+++ projects/antra-ui/src/lib/components/login/login.component.spec.ts
@@ -5,7 +5,7 @@ import { AntraUiModule } from '../../antra-ui.module';
 import { By } from '@angular/platform-browser';
 
 @Component({
-  template: ` <antra-login (clickEvent)="getActionName($event)"></antra-login>`,
+  template: `<antra-login (loginActionEvent)="getActionName($event)"></antra-login>`,
 })
 class TestHostComponent {}
 
@@ -44,19 +44,26 @@ describe('LoginComponent', () => {
     expect(btnElement.textContent.trim()).toBe('LOGIN');
   });
 
-  it('should have button with text content LOGIN', () => {
-    const btnElement = fixture.nativeElement.querySelector('button');
-    expect(btnElement.textContent.trim()).toBe('LOGIN');
+  it('should have b element with textcontent Login using', () => {
+    // const boldElement = fixture.nativeElement.querySelector('b');
+    // expect(boldElement.textContent.trim()).toBe('Login using');
+
+    const boldElement = fixture.debugElement.query(By.css('b'));
+    expect(boldElement.nativeElement.innerText).toBe('Login using');
   });
 
-  it('should have b element with textcontent Login using ', () => {
-    const boldElement = fixture.nativeElement.querySelector('b');
-    expect(boldElement.textContent.trim()).toBe('Login using');
-    expect(boldElement.className).toContain('light-color');
-  });
+  // it('should have class with the name widget1 for article element', () => {
+  //   const article: HTMLElement = fixture.nativeElement.querySelector('article');
+  //   expect(article.className).toContain('widget1');
+  // });
 
-  it('should have img element with classname btn-img ', () => {
+  // it(`should have a p tag of innerText 'customize style with className' `, () => {
+  //   const p = fixture.debugElement.query(By.css('p'));
+  //   expect(p.nativeElement.innerText).toBe('customize style with className');
+  // });
+
+  it('should have img element with classname form__button-img', () => {
     const imgElement = fixture.nativeElement.querySelector('img');
-    expect(imgElement.className).toContain('btn-img');
+    expect(imgElement.className).toContain('form__button-img');
   });
 });
