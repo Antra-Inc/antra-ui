a49e0b380d87c9cdd2e9ec55acba52bea3c0c1c7
narendarlgentyala-antra <narendar.gentyala@antra.com>
2021-01-12 17:25:05 +0530

    Feature - login component update - test cases

--- projects/antra-ui/src/lib/components/login/login.component.spec.ts
+++ projects/antra-ui/src/lib/components/login/login.component.spec.ts
@@ -32,38 +32,24 @@ describe('LoginComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should have event binding attribute with the name loginActionEvent', () => {
-    const loginElement = fixture.debugElement.query(By.css('antra-login'));
-
-    // tslint:disable-next-line: no-unused-expression
-    expect(loginElement.nativeElement.attributes).toContain('loginActionEvent');
-  });
-
   it('should have button with text content LOGIN', () => {
     const btnElement = fixture.nativeElement.querySelector('button');
     expect(btnElement.textContent.trim()).toBe('LOGIN');
   });
 
-  it('should have b element with textcontent Login using', () => {
-    // const boldElement = fixture.nativeElement.querySelector('b');
-    // expect(boldElement.textContent.trim()).toBe('Login using');
-
-    const boldElement = fixture.debugElement.query(By.css('b'));
-    expect(boldElement.nativeElement.innerText).toBe('Login using');
+  it('should have form with class name login-form', () => {
+    const frmElement = fixture.nativeElement.querySelector('form');
+    expect(frmElement.className).toContain('login-form');
   });
 
-  // it('should have class with the name widget1 for article element', () => {
-  //   const article: HTMLElement = fixture.nativeElement.querySelector('article');
-  //   expect(article.className).toContain('widget1');
-  // });
-
-  // it(`should have a p tag of innerText 'customize style with className' `, () => {
-  //   const p = fixture.debugElement.query(By.css('p'));
-  //   expect(p.nativeElement.innerText).toBe('customize style with className');
-  // });
-
-  it('should have img element with classname form__button-img', () => {
-    const imgElement = fixture.nativeElement.querySelector('img');
-    expect(imgElement.className).toContain('form__button-img');
+  it('should have input with class name form__input', () => {
+    const frmElement = fixture.nativeElement.querySelector('input');
+    expect(frmElement.className).toContain('form__input');
   });
+
+  it('should have div with class names form-container antra-widget', () => {
+    const frmElement = fixture.nativeElement.querySelector('div');
+    expect(frmElement.className).toContain('form-container antra-widget');
+  });
+
 });
