e754906cd6c43251a82caa30420e70d5089028bd
narendarlgentyala-antra <narendar.gentyala@antra.com>
2021-01-14 18:24:16 +0530

    Feature - Antra Password Reset Successful Screen

--- projects/antra-ui/src/lib/components/password-reset-successful/password-reset-successful.component.spec.ts
+++ projects/antra-ui/src/lib/components/password-reset-successful/password-reset-successful.component.spec.ts
@@ -1,25 +1,47 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Component } from '@angular/core';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { AntraUiModule } from '../../antra-ui.module';
 
 import { PasswordResetSuccessfulComponent } from './password-reset-successful.component';
 
-describe('PasswordResetSuccessfulComponent', () => {
-  let component: PasswordResetSuccessfulComponent;
-  let fixture: ComponentFixture<PasswordResetSuccessfulComponent>;
+@Component({
+  template: `<antra-password-reset-successful
+    class="default"
+    [successfulMessage]="successfulMessage1"
+  ></antra-password-reset-successful>`,
+})
+class TestHostComponent {}
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ PasswordResetSuccessfulComponent ]
+describe('PasswordResetSuccessfulComponent', () => {
+  let component: TestHostComponent;
+  let fixture: ComponentFixture<TestHostComponent>;
+
+  beforeEach(
+    waitForAsync(() => {
+      TestBed.configureTestingModule({
+        declarations: [PasswordResetSuccessfulComponent, TestHostComponent],
+        imports: [AntraUiModule],
+      }).compileComponents();
     })
-    .compileComponents();
-  });
+  );
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(PasswordResetSuccessfulComponent);
+    fixture = TestBed.createComponent(TestHostComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have span with text content check_circle', () => {
+    const spanElement = fixture.nativeElement.querySelector('span');
+    expect(spanElement.textContent.trim()).toBe('check_circle');
+  });
+
+  it('should have div with class names form-container antra-widget', () => {
+    const divElement = fixture.nativeElement.querySelector('div');
+    expect(divElement.className).toContain('form-container antra-widget');
   });
 });
