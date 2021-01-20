f376897280a9270d14c078eae89db478096faae5
narendarlgentyala-antra <narendar.gentyala@antra.com>
2021-01-20 22:24:12 +0530

    Login Component - Error Messages fixed

--- src/app/components/pages/login-page/login-example/login-example.component.ts
+++ src/app/components/pages/login-page/login-example/login-example.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginActions } from 'antra-ui/lib/interfaces/login.interface';
 
 @Component({
   selector: 'app-login-example',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login-example.component.scss'],
 })
 export class LoginExampleComponent implements OnInit {
-  actionNameOne: string;
-  actionNameTwo: string;
-  actionNameThree: string;
-  actionNameFour: string;
+  actionNameOne: LoginActions;
+  actionNameTwo: LoginActions;
+  actionNameThree: LoginActions;
+  actionNameFour: LoginActions;
+
+  loginErrorMessage1: string;
+  showLoginError1 = false;
+
+  loginErrorMessage2: string;
+  showLoginError2 = false;
 
   passwordPattern = '^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$';
   passwordPattern1 = '^(?=.*?[A-Z])(?=.*?[a-z]).{8,}$';
@@ -33,19 +40,60 @@ export class LoginExampleComponent implements OnInit {
   ngOnInit(): void {}
 
   // tslint:disable-next-line: typedef
-  getActionNameOne(details: string) {
-    this.actionNameOne = details;
+  getActionNameOne(details: LoginActions) {
+    this.showLoginError1 = false;
+    if (
+      details.actionType === 'ForgotPassword' ||
+      details.actionType === 'GoogleLogin' ||
+      details.actionType === 'MsftLogin'
+    ) {
+      this.actionNameOne = details;
+    } else if (
+      details.actionType === 'Login' &&
+      details.email === 'admin@gmail.com' &&
+      details.password === 'Password@123'
+    ) {
+      this.actionNameOne = details;
+      this.showLoginError1 = false;
+      this.loginErrorMessage1 = '';
+    } else {
+      this.showLoginError1 = true;
+      this.loginErrorMessage1 = 'Invalid Credentials';
+      this.actionNameOne = {
+        actionType: details.actionType,
+        email: details.email,
+        password: details.password,
+      };
+    }
   }
   // tslint:disable-next-line: typedef
-  getActionNameTwo(details: string) {
+  getActionNameTwo(details: LoginActions) {
     this.actionNameTwo = details;
   }
   // tslint:disable-next-line: typedef
-  getActionNameThree(details: string) {
+  getActionNameThree(details: LoginActions) {
     this.actionNameThree = details;
   }
+
+  registeredEmailIds = ['ramesh@gmail.com', 'rajeev@gmail.com', 'narend@gmail.com'];
   // tslint:disable-next-line: typedef
-  getActionNameFour(details: string) {
-    this.actionNameFour = details;
+  getActionNameFour(details: LoginActions) {
+    this.showLoginError2 = false;
+    for (let email of this.registeredEmailIds) {
+      if (email === details.email) {
+        this.actionNameFour = details;
+        this.showLoginError2 = false;
+        this.loginErrorMessage2 = '';
+        break;
+      } else {
+        this.showLoginError2 = true;
+        this.loginErrorMessage2 = 'Email Id does not exist';
+        this.actionNameFour = {
+          actionType: details.actionType,
+          email: details.email,
+          password: details.password,
+        };
+      }
+    }
   }
 }
